cd5ae7a6743f0a8c4b9c8cc3a8352e25ff34ae67
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-08 12:07:31 +0200

    Primer commit

---
+++ Simple_Class/src/Edad.java
@@ -0,0 +1,52 @@
+//---------LA TEORÍA-------------
+//Cada archivo .java se denomina clase
+
+//SECUENCIA DE COLORES:
+	//NARANJA = Palabras clave de Java
+	//AZUL = Nombre clase (Edad)
+	//CIAN = Nombre variable privada (Años)
+	//EL OTRO AZUL = Nombre variable no privada (A)
+	//VERDE = Nombre de método al crear (setAños, getAños, FaltanParaCien) (Líneas: 30 39 48)
+	//LIMA = Nombre de método al llamarlo (getAños) (Línea 49)
+
+//PRIVATE/PUBLIC
+	//PRIVATE: Variables / métodos que se utilizan sólo desde esta clase. No se pueden llamar desde otra. (Variable Años)
+	//PUBLIC: Variables / métodos que se utilizan desde esta y otra clase. Se pueden utilizar desde otra clase porque son Public (setAños, getAños, FaltanParaCien)
+
+//OPERACIONES:
+	//Años es de tipo short	
+	//El método FaltanParaCien() devuelve un tipo int
+		//Se opera 100-getAños (int-short). Esta operación es posible porque int>short
+	//Secuencia de tipos: byte>short>int>long>float>double
+
+//------------COMIENZO DEL PROGRAMA---------------
+public class Edad {		//De esta forma se inicializa una clase. Esta clase se llama Edad.
+
+//------------INICIALIZACIÓN DE VARIABLES----------------
+	private short Años; //De esta forma se inicializa una variable (atributo) de una clase. (private/public) (tipo dato) (nombre)
+						//En todos los programas, las variables serán de tipo private.
+
+//---------------MÉTODOS SET-----------------------
+	public void setAños(short A) {		//De esta forma se inicializa un método de una clase. (private/public) (tipo de dato que devuelve) (nombre) (variables que necesita)
+										//En todos los programas, los métodos serán de tipo public.
+										//Este método se llama setAños
+										//Este método devuelve un valor de tipo void (vacío) o sea, no devuelve nada
+										//Este método requiere una variable de tipo short a la que llamaremos A
+		Años = A;		//Esta es la función del método. Introducirá el valor de A en la variable Años.		
+	}		//Cerramos el método con una llave
+	
+//-----------------MÉTODOS GET-------------------
+	public short getAños () {			//Método getAños. Devolverá los años introducidos.
+										//Este método es de tipo public
+										//Este método devuelve un valor de tipo short (número entero) por lo que después de public dice short
+										//Este método no requiere ninguna variable así que entre los paréntesis no hay nada escrito.
+		return Años;		//La función return es NECESARIA en un método que devuelva un valor. La forma de la función es: return (variable)
+							//NOTA: EL TIPO DE VARIABLE DEBE SER EL MISMO QUE APAREZCA EN LA INICIALIZACIÓN DEL MÉTODO. EN ESTE CASO SHORT.
+	}		//Cerramos el método con una llave
+	
+//----------------MÉTODOS DE CÁLCULO--------------------
+	public int FaltanParaCien () {		//Método FaltanParaCien. Devolverá los años restantes para los 100
+		return (100 - getAños());		//Devuelve el valor de 100 - los Años introducidos
+										//NOTA: EN ESTE MÉTODO ESTAMOS LLAMANDO A OTRO MÉTODO (GETAÑOS) PARA QUE NOS DEVUELVA LA VARIABLE AÑOS.		
+	}		//Cerramos el método con una llave
+}
---
+++ Simple_Class/src/EdadPrueba.java
@@ -0,0 +1,55 @@
+//---------LA TEORÍA-------------
+//Cada archivo .java se denomina clase
+
+//SECUENCIA DE COLORES:
+	//NARANJA = Palabras clave de Java
+	//AZUL = Nombre clase (EdadPrueba, Scanner, Edad)
+	//AMARILLO = Nombre de objeto al crear (teclado, alex, Napoleón) (Líneas: 26 27 39)
+	//EL OTRO AMARILLO = Nombre de objeto al llamar (teclado, alex, Napoleón) (Líneas: 30 33 34 42 44 45)
+	//VERDE = Nombre de método al crear (main) (Líneas: 24)
+	//LIMA = Nombre de método al llamarlo (setAños, nextShort, getAños, FaltanParaCien) (Líneas: 30 33 34 42 44 ...)
+
+//PRINT()
+	//printf("Texto %(letra)",(Variable/método/operación)) Imprimirá el texto entre comillas sustituyendo el %(letra) por el valor a la derecha de las comillas y la coma.
+	//En este caso imprimiremos valores enteros (años) así que la letra debe ser d (%d)
+	//Si imprimiésemos valores reales (con decimales) la letra debería ser la f (%f)
+
+	//Si queremos imprimir dos líneas deberemos añadir \n al final de la primera: printf("texto %d.\n",variable)
+	//																			  printf("texto2 %d.",variable2)
+
+//Esquemas:
+	//Crear objeto: TIPO NOMBRE = new TIPO ();
+	//Llamar métodos: OBJETO.MÉTODO(VALORES); (valores sí es necesario)
+	//printf ("texto %(letra)",valor);
+
+
+//------------COMIENZO DEL PROGRAMA--------------------
+import java.util.Scanner; 		//IMPORTAR CLASE SCANNER PARA VARIABLE teclado
+public class EdadPrueba {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner (System.in);		//CREAR OBJETO DE TIPO SCANNER LLAMADO TECLADO.
+		Edad alex = new Edad ();		//CREAR OBJETO DE TIPO EDAD LLAMADO ALEX
+		
+		System.out.print("Introduzca su edad: "); 		//SOLICITA EDAD
+		alex.setAños(teclado.nextShort());		//LLAMAMOS MÉTODO SETAÑOS() DE OBJETO ALEX Y LE ENTREGAMOS COMO VARIABLE (A) LO QUE INTRODUZCA EL USUARIO POR EL TECLADO
+							//NOTA: ESTAMOS LLAMANDO UNA FUNCIÓN (SETAÑOS()) Y PARA ENTREGARLE LA VARIABLE QUE PRECISA ESTAMOS LLAMANDO OTRO MÉTODO (NEXTSHORT()) DEL OBJETO TECLADO
+		
+		System.out.printf("Tienes %d años.\n",alex.getAños());		//IMPRIMIMOS VALOR INTRODUCIDO POR EL USUARIO UTILIZANDO PRINTF(). LLAMAREMOS AL MÉTODO (GETAÑOS()) DEL OBJETO ALEX
+		System.out.printf("Te faltan %d años para llegar a 100.\n",alex.FaltanParaCien());		//IMPRIMIMOS LOS AÑOS RESTANTES PARA LLEGAR A LOS 100. LLAMAREMOS AL MÉTODO (FALTANPARACIEN()) DEL OBJETO ALEX
+	
+		//Ahora repetiremos el ejercicio con otra edad.
+		//VAMOS A ALMACENAR DOS EDADES A LA VEZ, UNA BAJO EL NOMBRE DE ALEX Y OTRA BAJO NAPOLEÓN.
+		
+		Edad Napoleón = new Edad(); 		//CREAMOS OTRO OBJETO DE TIPO EDAD LLAMADO NAPOLEÓN.
+		
+		System.out.print("Introduzca otra edad: ");		//SOLICITAMOS OTRO VALOR
+		Napoleón.setAños(teclado.nextShort());		//LLAMAMOS MÉTODO SETAÑOS() DE OBJETO NAPOLEÓN Y LE ENTREGAMOS COMO VARIABLE (A) LO QUE INTRODUZCA EL USUARIO POR EL TECLADO
+		
+		System.out.printf("Si tiene %d años, faltan %d para los 100\n", Napoleón.getAños(),Napoleón.FaltanParaCien());		//MOSTRAMOS LOS AÑOS Y LOS QUE FALTAN PARA 100, UTILIZAREMOS DOS %d Y DOS MÉTODOS DEL OBJETO NAPOLEÓN
+		System.out.printf("La diferencia entre los años introducidos es de %d.",Math.abs(alex.getAños()-Napoleón.getAños()));		//MOSTRAMOS LA DIFERENCIA ENTRE LOS AÑOS HACIENDO UNA OPERACIÓN DE DOS MÉTODOS
+														//Llamamos al método abs() de la clase Math (preexistente en java) para obtener el valor absoluto de la resta. (evitar resultados negativos)
+	}		
+
+}
